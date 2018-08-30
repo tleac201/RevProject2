fdb1c2c8c38a846af72c2b7d935ebaaa08f24025
Tyler <tyler.leach1995@gmail.com>
2018-08-29 23:34:46 -0400

    Added StandardProductsController

---
+++ Project2/Controllers/StandardProductsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Project2;
+
+namespace Project2.Controllers
+{
+    public class StandardProductsController : ApiController
+    {
+        private Project2Entities db = new Project2Entities();
+
+        // GET: api/StandardProducts
+        public IQueryable<StandardProduct> GetStandardProducts()
+        {
+            return db.StandardProducts;
+        }
+
+        // GET: api/StandardProducts/5
+        [ResponseType(typeof(StandardProduct))]
+        public IHttpActionResult GetStandardProduct(int id)
+        {
+            StandardProduct standardProduct = db.StandardProducts.Find(id);
+            if (standardProduct == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(standardProduct);
+        }
+
+        // POST: api/StandardProducts
+        [ResponseType(typeof(StandardProduct))]
+        public IHttpActionResult PostStandardProduct(StandardProduct standardProduct)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.StandardProducts.Add(standardProduct);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = standardProduct.Id }, standardProduct);
+        }
+    }
+}
